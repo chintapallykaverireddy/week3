a1cf86b5ab849ec99115b73096140cd504f2f23a
kaverireddy <kaverireddy267@gmail.com>
2024-11-21 21:59:20 +0530

    commit-3

--- Prime.java
+++ Prime.java
@@ -5,8 +5,10 @@ import java.lang.;
 class Prime {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+//taking input
         int n = sc.nextInt();
         int c = 0;
+//checking prime 
         for (int i = 2; i <= Math.sqrt(n); i++) {
             if (n % i == 0) {
                 c++;
